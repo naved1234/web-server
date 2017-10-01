43823450e5001da66590231f03d71ada0084f423
Naved <naved1234@gmail.com>
2017-10-01 11:53:57 +0530

    Changes saved to server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var middleware=require('./middleware.js')
 app.use(middleware.logger);
 app.get('/about',middleware.requireAuthentication,function(req,res)
 {
-  res.send('About us !');
+  res.send('It\'s about us!');
 });
 app.use(express.static(__dirname+'/public'));
 app.listen(PORT,function()
