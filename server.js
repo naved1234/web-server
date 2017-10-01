4e5872806f47f7d8701973121d5f89b9cfbc17ce
Naved <naved1234@gmail.com>
2017-10-01 12:35:46 +0530

    just changed the res.send comment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var middleware=require('./middleware.js')
 app.use(middleware.logger);
 app.get('/about',middleware.requireAuthentication,function(req,res)
 {
-  res.send('It\'s about us!');
+  res.send('It\'s all about us!');
 });
 app.use(express.static(__dirname+'/public'));
 app.listen(PORT,function()
